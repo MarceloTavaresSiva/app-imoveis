da497f865157c18681635803b760aab37f4dc4db
adilson-tavares <tavares.adilson.c@gmail.com>
2023-12-30 16:31:00 -0400

    feat: Adiciona middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,32 @@
+const User = require('../models/User')
+// backend/middleware/auth.js
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, 'nossosecret', (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+      } else {
+        req.userId = decodedToken.userId;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'No token provided' });
+  }
+};
+
+const checkRole = (roles) => async (req, res, next) => {
+    let id = req.params.id;
+
+    //retrieve employee info from DB
+    const user = await User.findById(id);
+    
+    !roles.includes(user.role) ? res.status(401).json({message: "Sorry you do not have access to this route"})
+      : next();
+  };
+
+module.exports = {requireAuth,  checkRole};
